68ad13b85abdcf0956244df973be98b1292aefca
Casto Gil <casto782@gmail.com>
2023-12-15 01:34:59 -0300

    actualizacion

--- src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
+++ src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
@@ -33,28 +33,28 @@ public class NuevoOradorController extends HttpServlet {
 
 	     List<Orador> oradores = oradorRepository.findAll();
 
-	     // Convierte la lista de oradores a JSON y envíala como respuesta
+	    
 	     ObjectMapper objectMapper = new ObjectMapper();
 	     resp.getWriter().write(objectMapper.writeValueAsString(oradores));
 	 }
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	    // Obtener datos del cuerpo de la solicitud (JSON)
+	   
 	    String json = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 
-	    // Puedes imprimir el JSON para verificar que se esté recibiendo correctamente
+	    
 	    System.out.println("JSON recibido: " + json);
 
-	    // Puedes utilizar una librería como Jackson para convertir el JSON a un objeto Java
+	  
 	    ObjectMapper objectMapper = new ObjectMapper();
 	    OradorRequest oradorRequest = objectMapper.readValue(json, OradorRequest.class);
 
-	    // Conectar con el repositorio y guardar el orador
+	  
 	    AdministradorDeConexiones administradorDeConexiones = new AdministradorDeConexiones();
 	    Connection conexion = administradorDeConexiones.getConnection();
 	    OradorRepository oradorRepository = new MysqlOradorRepository(conexion);
-	    // Convertir OradorRequest a Orador
+	    
 	    Orador nuevoOrador = new Orador(
 	            oradorRequest.getNombre(),
 	            oradorRequest.getApellido(),
@@ -69,10 +69,10 @@ public class NuevoOradorController extends HttpServlet {
 	}
 	 @Override
 	    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	        // Obtener el ID del orador de la URL
+	     
 		 	String id = req.getParameter("id");
 
-	        // Conectar con el repositorio y eliminar el orador
+	        
 	        AdministradorDeConexiones administradorDeConexiones = new AdministradorDeConexiones();
 	        Connection conexion = administradorDeConexiones.getConnection();
 	        OradorRepository oradorRepository = new MysqlOradorRepository(conexion);
@@ -87,13 +87,13 @@ public class NuevoOradorController extends HttpServlet {
 	 protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	     String id = req.getParameter("id");
 	     if (id != null) {
-	         // Obtener datos del cuerpo de la solicitud (JSON)
+	        
 	         String json = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 
 	         ObjectMapper objectMapper = new ObjectMapper();
 	         OradorRequest oradorRequest = objectMapper.readValue(json, OradorRequest.class);
 
-	         // Conectar con el repositorio y actualizar el orador
+	        
 	         AdministradorDeConexiones administradorDeConexiones = new AdministradorDeConexiones();
 	         Connection conexion = administradorDeConexiones.getConnection();
 	         OradorRepository oradorRepository = new MysqlOradorRepository(conexion);
@@ -101,13 +101,13 @@ public class NuevoOradorController extends HttpServlet {
 	         int oradorId = Integer.parseInt(id);
 	         Orador oradorExistente = oradorRepository.getById(oradorId);
 
-	         // Actualizar los campos del oradorExistente con los nuevos datos
+	         
 	         oradorExistente.setNombre(oradorRequest.getNombre());
 	         oradorExistente.setApellido(oradorRequest.getApellido());
 	         oradorExistente.setEmail(oradorRequest.getEmail());
 	         oradorExistente.setTema(oradorRequest.getTema());
 
-	         // Guardar el orador actualizado
+	         
 	         oradorRepository.update(oradorExistente);
 
 	         resp.getWriter().write(objectMapper.writeValueAsString(oradorExistente));
